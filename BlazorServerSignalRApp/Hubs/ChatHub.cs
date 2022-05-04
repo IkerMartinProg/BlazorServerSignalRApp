b153f121389655123eb7bcd585cd2e3cfafc22de
IkerMartin <ikermartinprog@gmail.com>
2022-05-04 15:28:08 +0200

    Versión con comentarios

--- BlazorServerSignalRApp/Hubs/ChatHub.cs
+++ BlazorServerSignalRApp/Hubs/ChatHub.cs
@@ -4,6 +4,13 @@ namespace BlazorServerSignalRApp.Hubs
 {
     public class ChatHub : Hub
     {
+        /* 
+            El método "SendMessage" recibe dos campos: el nombre del usuario 
+            y su correspondiente mensaje.
+            El contenido, anteriormente mencionado, se enviará a todas las
+            ventanas que hayan accedido al chat común.
+        */
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
